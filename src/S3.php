9bc4792d8773f2a7fd1799d2cd143b4bf11d58ac
robwilson <rob.wilson@supergroup.co.uk>
2019-01-28 10:04:58 +0000

    Init Commit

---
+++ src/S3.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace devtoolboxuk\aws;
+
+use \Aws\S3\S3Client;
+
+/**
+ * Class S3
+ * Simple Class to connect to S3.
+ * @package SuperGroup
+ */
+class S3
+{
+    /**
+     * @var object
+     */
+    protected $s3Client;
+
+    /**
+     * @var
+     */
+    protected $bucket;
+
+    function __construct()
+    {
+        $this->s3Client = S3Client::factory();
+    }
+
+    public function setBucket($bucket)
+    {
+        $this->bucket = $bucket;
+    }
+
+    public function getBucket()
+    {
+        return $this->bucket;
+    }
+
+    /**
+     * @param $localFilename
+     * @param $s3filename
+     * @param string $acl
+     */
+    public function putObject($localFilename, $s3filename, $acl = 'bucket-owner-full-control')
+    {
+        $data = [
+            'ACL' => $acl,
+            'Bucket' => $this->bucket,
+            'Key' => $s3filename,
+            'SourceFile' => $localFilename
+        ];
+
+        try {
+            $this->s3Client->putObject($data);
+        } catch (S3Exception $e) {
+            throw new \S3Exception(sprintf("Failed to upload file '%s' to S3.", $s3filename));
+        }
+    }
+
+    public function getObject($localFilename, $s3filename)
+    {
+
+        $data = [
+            'Bucket' => $this->bucket,
+            'Key' => $s3filename,
+            'SaveAs' => $localFilename
+        ];
+
+        try {
+            $this->s3Client->getObject($data);
+        } catch (S3Exception $e) {
+            throw new RuntimeException(sprintf("Failed to get file '%s' from S3.", $s3filename));
+        }
+    }
+
+    public function removeObject($s3filename)
+    {
+        try {
+            $this->s3Client->deleteObject([
+                'Bucket' => $this->bucket,
+                'Key' => $s3filename
+            ]);
+        } catch (S3Exception $e) {
+            throw new RuntimeException(sprintf("Failed to delete file '%s' from S3.", $s3filename));
+        }
+    }
+
+}
